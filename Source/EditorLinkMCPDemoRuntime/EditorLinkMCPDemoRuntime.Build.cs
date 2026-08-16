52a938de724a1f86319d0b800c95b778b4b97e50
Bruno <brunokim@gmail.com>
2026-08-16 15:06:14 -0300

    Add Source/EditorLinkMCPDemoRuntime/EditorLinkMCPDemoRuntime.Build.cs

---
+++ Source/EditorLinkMCPDemoRuntime/EditorLinkMCPDemoRuntime.Build.cs
@@ -0,0 +1,17 @@
+using UnrealBuildTool;
+
+public class EditorLinkMCPDemoRuntime : ModuleRules
+{
+	public EditorLinkMCPDemoRuntime(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine"
+		});
+	}
+}
+
