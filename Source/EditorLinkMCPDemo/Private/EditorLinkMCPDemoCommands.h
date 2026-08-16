fa14bd3a51d0fe82a7fc43872bcdbe8b4f1507a4
Bruno <brunokim@gmail.com>
2026-08-16 15:05:53 -0300

    Add Source/EditorLinkMCPDemo/Private/EditorLinkMCPDemoCommands.h

---
+++ Source/EditorLinkMCPDemo/Private/EditorLinkMCPDemoCommands.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+class FJsonObject;
+
+class FEditorLinkMCPDemoCommands
+{
+public:
+	static TSharedPtr<FJsonObject> Execute(const FString& Command, const TSharedPtr<FJsonObject>& Parameters);
+
+private:
+	static TSharedPtr<FJsonObject> MakeSuccess(const TSharedPtr<FJsonObject>& Data = nullptr);
+	static TSharedPtr<FJsonObject> MakeError(const FString& Error, const FString& Code = TEXT("command_failed"));
+};
+
+
