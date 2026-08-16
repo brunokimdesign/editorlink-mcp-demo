bc295ca3af5736603e73edc6eae4ff03b3356d84
Bruno <brunokim@gmail.com>
2026-08-16 15:05:35 -0300

    Add Source/EditorLinkMCPDemo/EditorLinkMCPDemo.Build.cs

---
+++ Source/EditorLinkMCPDemo/EditorLinkMCPDemo.Build.cs
@@ -0,0 +1,41 @@
+using UnrealBuildTool;
+
+public class EditorLinkMCPDemo : ModuleRules
+{
+	public EditorLinkMCPDemo(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new[]
+		{
+			"ApplicationCore",
+			"AssetRegistry",
+			"AssetTools",
+			"BlueprintGraph",
+			"ContentBrowser",
+			"EditorSubsystem",
+			"EditorLinkMCPDemoRuntime",
+			"HTTPServer",
+			"InputCore",
+			"Json",
+			"JsonUtilities",
+			"KismetCompiler",
+			"MessageLog",
+			"Projects",
+			"Settings",
+			"Slate",
+			"SlateCore",
+			"ToolMenus",
+			"UnrealEd",
+			"WorkspaceMenuStructure"
+		});
+	}
+}
+
