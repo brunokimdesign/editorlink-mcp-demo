b754f41684ce7c9474359ff3c3acf999475a8827
Bruno <brunokim@gmail.com>
2026-08-16 15:06:04 -0300

    Add Source/EditorLinkMCPDemo/Public/EditorLinkMCPDemo.h

---
+++ Source/EditorLinkMCPDemo/Public/EditorLinkMCPDemo.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Modules/ModuleManager.h"
+
+class FEditorLinkMCPDemoModule final : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+private:
+	TSharedRef<class SDockTab> SpawnEditorLinkTab(const class FSpawnTabArgs& Args);
+	void OpenEditorLinkTab();
+	void RegisterMenus();
+};
+
